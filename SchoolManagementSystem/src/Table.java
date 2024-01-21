bcc82778c1e57274687b1ca1c0e727d810305710
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-21 17:30:13 -0500

    Created new child class of Table.

--- SchoolManagementSystem/src/Table.java
+++ SchoolManagementSystem/src/Table.java
@@ -22,14 +22,4 @@ public class Table {
 		}
 		return fieldString;
 	}
-	
-	/*
-	 * This function will load table with data from CSV
-	 * First, it will set the fields = fields in csv
-	 * Then, it will set data like:
-	 * {field1: [value, ...], field2: [value, ...], ...}
-	 */
-	public void loadFromCSV(String filepath) {
-		
-	};
 }
---
+++ SchoolManagementSystem/src/TeacherTable.java
@@ -0,0 +1,4 @@
+
+public class TeacherTable extends Table {
+
+}
